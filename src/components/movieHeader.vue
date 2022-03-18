b9ee95ecd6c02ba970fed198b72c2bb3c5762bd6
Siripiri-hue <daemonprocellae@live.it>
2022-03-18 16:44:56 +0100

    Aggiunto header

---
+++ src/components/movieHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <h1>Boolflix</h1>
+
+        <div id="search">
+            <input type="text" placeholder="Type to search">
+            <button>Search</button>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Ma+Shan+Zheng&display=swap');
+
+header {
+    font-family: 'Ma Shan Zheng', cursive;
+    color: #E6271F;
+    display: flex;
+    justify-content: space-around;
+    height: 10vh;
+    background-color: rgb(75, 75, 75);
+    font-size: 20px;
+    position: fixed;
+    width: 100%;
+    padding: 15px 10px;
+
+    #search {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+}
+
+</style>
